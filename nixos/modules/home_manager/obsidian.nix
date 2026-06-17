2868a643d298f797b24aa176512ede8d6948a505
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-17 19:47:23 +0800

    reverted obsidian config

--- nixos/modules/home_manager/obsidian.nix
+++ nixos/modules/home_manager/obsidian.nix
@@ -1,23 +1,68 @@
+{ lib, ... }:
+let
+  obsidian_path = "../../../../configs/.obsidian";
+  obsidian_vault = "./Documents/obsidian_vault";
+in
 {
   programs.obsidian.enable = true;
 
   programs.obsidian.vaults."Obsidian Vault" = {
     enable = true;
-    target = "./Documents/obsidian_vault";
+    target = "${obsidian_vault}";
+
+    # settings = {
+    #   cssSnippets."default" = {
+    #     enable = true;
+    #     text = ''
+    #       .markdown-preview-view ul {
+    #         margin-block-start: -0.7em;
+    #         /* margin-block-end: -0.7em; */
+    #       }
+
+    #       .markdown-preview-view ol {
+    #         margin-block-start: -0.7em;
+    #         /* margin-block-end: -0.7em; */
+    #       }
+
+    #       .markdown-preview-view p {
+    #         margin-block-start: 1em;
+    #       }
+
+    #       .cm-line:has(+ .cm-embed-block.cm-table-widget) br {
+    #         display: none;
+    #       }
+    #     '';
+    #   };
+
+    #   app = ./app.json;
+    #   appearance = ./appearance.json;
+    #   hotkeys = ./hotkeys.json;
+    # };
   };
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # "Documents/obsidian_vault/.obsidian".source = "../../../configs/.obsidian";
+  # home.file = {
+  #   "obsidian_bookmarks" = {
+  #     source = "${obsidian_path}/bookmarks.json";
+  #     target = "${obsidian_vault}/.obsidian/bookmarks.json";
+  #   };
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
+  #   "obsidian_community_plugins" = {
+  #     recursive = true;
+  #     source = "${obsidian_path}/plugins";
+  #     target = "${obsidian_vault}/.obsidian/plugins";
+  #   };
+
+  #   "obsidian_core_plugins" = {
+  #     source = "${obsidian_path}/core-plugins.json";
+  #     target = "${obsidian_vault}/.obsidian/core-plugins.json";
+  #   };
+
+  #   "obsidian_core_templates" = {
+  #     source = "${obsidian_path}/templates.json";
+  #     target = "${obsidian_vault}/.obsidian/templates.json";
+  #   };
+  # };
+
 }
