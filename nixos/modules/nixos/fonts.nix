b7090ad8333d0336c067aad58d60ce04ff73c5fc
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-18 14:53:57 +0800

    "98 2026-06-18 14:53:55 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/nixos/fonts.nix
+++ nixos/modules/nixos/fonts.nix
@@ -3,6 +3,8 @@
   # Install fonts (https://wiki.nixos.org/wiki/Fonts)
   fonts.packages = with pkgs; [
     cascadia-code
+    nerd-fonts.caskaydia-cove
+    nerd-fonts.caskaydia-mono
     nerd-fonts.iosevka
   ];
 }
