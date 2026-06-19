f145955f1fed28614b27bb91eb47c6186cdfbc95
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-19 19:15:17 +0800

    "113 2026-06-19 19:15:15 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/nixos/steam.nix
+++ nixos/modules/nixos/steam.nix
@@ -1,7 +1,20 @@
-{
+{ config, pkgs, ... }: {
   programs.steam = {
     enable = true;
+    gamescopeSession.enable = true;
   };
-
   programs.gamemode.enable = true;
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = false; # see the note above
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+  hardware.nvidia.modesetting.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users."filo".packages = with pkgs; [
+    protonup-ng
+  ];
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+  };
 }
