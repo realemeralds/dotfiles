b7828ccccf7f62416c376c180bdc0fb9b1be6397
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-19 19:27:58 +0800

    "116 2026-06-19 19:27:51 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/home_manager/discord.nix
+++ nixos/modules/home_manager/discord.nix
@@ -1,3 +1,25 @@
-{
+{ pkgs, ... }: {
   programs.discord.enable = true;
+
+  # From ChatGPT
+  systemd.user.services.discord = {
+    Unit = {
+      Description = "Start Discord minimized";
+      After = [
+        "network-online.target"
+        "graphical-session.target"
+      ];
+      Wants = [ "network-online.target" ];
+    };
+
+    Service = {
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
+      ExecStart = "${pkgs.discord}/bin/discord --start-minimized";
+      Restart = "on-failure";
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 }
