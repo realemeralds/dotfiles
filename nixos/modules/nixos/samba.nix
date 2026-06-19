fbe30ba3f2d34a7389053f728295446da9aaf906
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-19 20:03:17 +0800

    "123 2026-06-19 19:58:35 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/nixos/samba.nix
+++ nixos/modules/nixos/samba.nix
@@ -1,30 +1,11 @@
 { pkgs, config, ... }:
-# {
-#   # For mount.cifs, required unless domain name resolution is not needed.
-#   environment.systemPackages = with pkgs; [
-#     cifs-utils
-#     samba
-#   ];
-#   fileSystems."/mnt/filofiles" = {
-#     device = "//192.168.1.69/filofiles";
-#     fsType = "cifs";
-#     options =
-#       let
-#         # this line prevents hanging on network split
-#         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-#       in
-#       [
-#         "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"
-#         "nofail"
-#       ];
-#   };
-#   networking.firewall.extraCommands = "iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns";
-# }
 {
   # For mount.cifs, required unless domain name resolution is not needed.
-  environment.systemPackages = [ pkgs.cifs-utils ];
-  fileSystems."/mnt/share" = {
+  environment.systemPackages = with pkgs; [
+    cifs-utils
+    samba
+  ];
+  fileSystems."/mnt/filofiles" = {
     device = "//192.168.1.69/filofiles";
     fsType = "cifs";
     options =
@@ -34,8 +15,27 @@
 
       in
       [
-        "${automount_opts},credentials=/etc/nixos/smb-secrets"
+        "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"
         "nofail"
       ];
   };
+  networking.firewall.extraCommands = "iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns";
 }
+# {
+#   # For mount.cifs, required unless domain name resolution is not needed.
+#   environment.systemPackages = [ pkgs.cifs-utils ];
+#   fileSystems."/mnt/share" = {
+#     device = "//192.168.1.69/filofiles";
+#     fsType = "cifs";
+#     options =
+#       let
+#         # this line prevents hanging on network split
+#         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+#       in
+#       [
+#         "${automount_opts},credentials=/etc/nixos/smb-secrets"
+#         "nofail"
+#       ];
+#   };
+# }
