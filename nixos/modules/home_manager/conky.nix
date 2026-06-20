bc631bc52d8c51d9c809f6ff8251cdb153ecc7f8
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-20 22:41:52 +0800

    "213 2026-06-20 22:41:49 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/home_manager/conky.nix
+++ nixos/modules/home_manager/conky.nix
@@ -1,12 +1,12 @@
 { pkgs, lib, ... }: {
   services.conky = {
     enable = true;
-    extraConfig = (builtins.readFile ./../../../configs/conky.conf);
+    # extraConfig = (builtins.readFile ./../../../configs/conky.conf);journalctl --user
   };
 
-  # home.file."conky" = {
-  #   enable = true;
-  #   source = ./../../../configs/conky.conf;
-  #   target = ".config/conky/conky.conf";
-  # };
+  home.file."conky" = {
+    enable = true;
+    source = ./../../../configs/conky.conf;
+    target = ".config/conky/conky.conf";
+  };
 }
