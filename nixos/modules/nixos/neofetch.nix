a4ed08431aeb99e84c0db0ae976c7e54ff489a5a
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-20 11:33:25 +0800

    "144 2026-06-20 11:33:22 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/nixos/neofetch.nix
+++ nixos/modules/nixos/neofetch.nix
@@ -5,8 +5,21 @@
 
   # 2. User-specific Home Manager Settings
   home-manager.users."filo" = { pkgs, ... }: {
-    programs.bash.shellAliases = {
-      neofetch = "hyfetch";
+    programs.zsh.shellAliases = {
+      neofetch = "neowofetch";
+    };
+
+    home.file."neofetch" = {
+      enable = true;
+      recursive = true;
+      source = ./../../../configs/neofetch;
+      target = ".config/neofetch";
+    };
+
+    home.file."hyfetch" = {
+      enable = true;
+      source = ./../../../configs/hyfetch.json;
+      target = ".config/hyfetch.json";
     };
 
     home.stateVersion = "26.11"; # Match your current state version
