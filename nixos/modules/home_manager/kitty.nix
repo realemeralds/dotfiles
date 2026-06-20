a69994b4aa7e69ca8c13bac4c9015e2291054d21
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-20 12:24:50 +0800

    "156 2026-06-20 12:24:48 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/home_manager/kitty.nix
+++ nixos/modules/home_manager/kitty.nix
@@ -9,11 +9,14 @@
       font_family = "CaskaydiaCove Nerd Font Mono";
       font_size = 14;
 
+      cursor = "#FFFFFF";
+      cursor_beam_thickness = 2;
       cursor_shape = "beam";
       cursor_shape_unfocused = "beam";
-      cursor_blink_interval = "0.5 ease-in-out";
+      cursor_blink_interval = "0.5";
 
       background_opacity = 0.9;
+      window_padding_width = "16 12 16 18";
     };
   };
 }
