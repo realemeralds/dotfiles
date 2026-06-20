1c6381aaebc92ec313648b3bfab253036b4d2f34
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-20 19:08:31 +0800

    "191 2026-06-20 19:08:27 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/home_manager/conky.nix
+++ nixos/modules/home_manager/conky.nix
@@ -2,4 +2,10 @@
   services.conky = {
     enable = true;
   };
+
+  home.file."conky" = {
+    enable = true;
+    source = ./../../../configs/conky.conf;
+    target = ".config/conky/conky.conf";
+  };
 }
