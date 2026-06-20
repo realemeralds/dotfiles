90c0ee21b7d074a97159f454f3164c91e60da116
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-20 20:47:08 +0800

    "192 2026-06-20 20:47:05 26.11.20260610.9ae611a 6.18.35"

--- nixos/hosts/default/configuration.nix
+++ nixos/hosts/default/configuration.nix
@@ -36,12 +36,7 @@
     ../../modules/nixos/steam.nix
   ];
 
-  # Use existing nixpkgs install (https://discourse.nixos.org/t/home-manager-does-not-allowunfree/25681/8)
-  home-manager.useGlobalPkgs = true;
-
-  # Bootloader.
-  # boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  networking.hostName = "nixos"; # Define your hostname.
 
   programs.nix-ld.enable = true;
   # programs.nix-ld.libraries = with pkgs; [
@@ -49,17 +44,10 @@
   #   # programs here, NOT in environment.systemPackages
   # ];
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
   ];
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -90,10 +78,6 @@
   services.xserver.desktopManager.cinnamon.enable = true;
   services.xserver.displayManager.lightdm.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  # services.displayManager.gdm.enable = true;
-  # services.desktopManager.gnome.enable = true;
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
@@ -112,11 +96,11 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    wireplumber.enable = true;
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
@@ -131,44 +115,15 @@
   };
 
   home-manager = {
-    # also pass inputs to home-manager modules
-    extraSpecialArgs = { inherit inputs; };
+    useGlobalPkgs = true; # Use existing nixpkgs install (https://discourse.nixos.org/t/home-manager-does-not-allowunfree/25681/8)
+    extraSpecialArgs = { inherit inputs; }; # also pass inputs to home-manager modules
     users = {
       "filo" = import ./home.nix;
     };
   };
 
-  # Install firefox.
-  # programs.firefox.enable = true;
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
