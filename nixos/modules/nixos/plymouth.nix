a4f82240e141a6293c63644aa9ae3d698ff61455
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-21 13:02:25 +0800

    "228 2026-06-21 13:02:23 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/nixos/plymouth.nix
+++ nixos/modules/nixos/plymouth.nix
@@ -1,43 +1,48 @@
 { pkgs, ... }: {
-  boot = {
-    # initrd.systemd.services.plymouth-start = {
-    #   after = [ "systemd-modules-load.service" ];
-    #   requires = [ "systemd-modules-load.service" ];
-    # };
-    plymouth = {
-      enable = true;
-      # theme = "lone";
-      # themePackages = with pkgs; [
-      #   # By default we would install all themes
-      #   (adi1090x-plymouth-themes.override {
-      #     selected_themes = [ "lone" ];
-      #   })
-      # ];
-      # theme = "rings";
-      # themePackages = with pkgs; [
-      #   # By default we would install all themes
-      #   (adi1090x-plymouth-themes.override {
-      #     selected_themes = [ "rings" ];
-      #   })
-      # ];
-    };
+  # boot = {
+  # initrd.systemd.services.plymouth-start = {
+  #   after = [ "systemd-modules-load.service" ];
+  #   requires = [ "systemd-modules-load.service" ];
+  # };
+  # plymouth = {
+  # enable = true;
+  # theme = "lone";
+  # themePackages = with pkgs; [
+  #   # By default we would install all themes
+  #   (adi1090x-plymouth-themes.override {
+  #     selected_themes = [ "lone" ];
+  #   })
+  # ];
+  # theme = "rings";
+  # themePackages = with pkgs; [
+  #   # By default we would install all themes
+  #   (adi1090x-plymouth-themes.override {
+  #     selected_themes = [ "rings" ];
+  #   })
+  # ];
+  # };
 
-    # Enable "Silent boot"
-    # consoleLogLevel = 3;
-    # initrd.verbose = false;
-    # initrd.kernelModules = [ "bochs" ];
-    initrd.systemd.enable = true;
-    kernelParams = [
-      # "quiet"
-      "boot.shell_on_fail"
-      # "rd.udev.log_level=3"
-      # "rd.systemd.show_status=auto"
-    ];
+  # Enable "Silent boot"
+  # consoleLogLevel = 3;
+  # initrd.verbose = false;
+  # initrd.kernelModules = [ "bochs" ];
+  # initrd.systemd.enable = true;
+  # kernelParams = [
+  # "quiet"
+  # "boot.shell_on_fail"
+  # "rd.udev.log_level=3"
+  # "rd.systemd.show_status=auto"
+  # ];
 
-    # Hide the OS choice for bootloaders.
-    # It's still possible to open the bootloader list by pressing any key
-    # It will just not appear on screen unless a key is pressed
-    # loader.timeout = 0;
-    loader.systemd-boot.enable = true;
-  };
+  # Hide the OS choice for bootloaders.
+  # It's still possible to open the bootloader list by pressing any key
+  # It will just not appear on screen unless a key is pressed
+  # loader.timeout = 0;
+  # loader.systemd-boot.enable = true;
+  # };
+
+  boot.plymouth.enable = true;
+  boot.plymouth.theme = "breeze";
+  boot.initrd.systemd.enable = true;
+  boot.loader.systemd-boot.enable = true;
 }
