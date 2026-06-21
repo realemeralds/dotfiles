3191119f895a50aecf20ea6bad1faacfdd6643e8
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-21 13:08:24 +0800

    "230 2026-06-21 13:07:00 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/nixos/steam.nix
+++ nixos/modules/nixos/steam.nix
@@ -5,10 +5,10 @@
   };
   programs.gamemode.enable = true;
 
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.open = false; # see the note above
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
-  hardware.nvidia.modesetting.enable = true;
+  # services.xserver.videoDrivers = [ "nvidia" ];
+  # hardware.nvidia.open = false; # see the note above
+  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+  # hardware.nvidia.modesetting.enable = true;
 
   environment.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
