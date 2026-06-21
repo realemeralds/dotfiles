7ff2e01347cdb7a9cbbe75ebe0008373a073eb0a
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-21 15:58:56 +0800

    "236 2026-06-21 15:58:54 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/nixos/syncthing.nix
+++ nixos/modules/nixos/syncthing.nix
@@ -2,6 +2,21 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
+    settings = {
+      devices = {
+        "syncthing-home" = {
+          id = "EW74H5F-TYZKS3U-IL6LYAL-TCS6ZUY-7ZBR2ZY-6C2SIDM-EVHGE2C-KRT4IAL";
+        };
+      };
+      folders = {
+        "obsidian_vault" = {
+          path = "/home/filo/Documents/obsidian_vault";
+          devices = [
+            "syncthing-home"
+          ];
+        };
+      };
+    };
   };
 
   # port 8384  is the default port to allow access from the network.
