b863f86e29a5581b08e0d9da5d8d23ee5effa854
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-23 23:15:58 +0800

    "272 2026-06-23 23:15:55 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/nixos/flatpak.nix
+++ nixos/modules/nixos/flatpak.nix
@@ -5,4 +5,8 @@
       "com.discordapp.Discord"
     ];
   };
+
+  home-manager.users."filo".home.sessionVariables = {
+    XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share";
+  };
 }
