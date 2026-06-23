9e6a893fa41f4ee9b03759b9ddbc70647e2f42ac
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-23 20:07:39 +0800

    "256 2026-06-23 20:07:37 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/nixos/sops.nix
+++ nixos/modules/nixos/sops.nix
@@ -2,6 +2,11 @@
   environment.systemPackages = with pkgs; [ sops ];
   sops.defaultSopsFile = ./secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
+  sops.age.keyFile = "/home/filo/.config/sops/age/keys.txt";
 
-  sops.age.keyFile = "/home/user/.config/sops/age/keys.txt";
+  sops.secrets."rclone.ini" = {
+    path = "/home/filo/.config/rclone/rclone.conf";
+    sopsFile = ../../secrets/rclone.ini;
+    format = "ini";
+  };
 }
