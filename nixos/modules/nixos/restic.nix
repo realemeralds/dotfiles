62eb7fcace810415e73e01ff37c0286fea6de255
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-23 20:12:39 +0800

    "258 2026-06-23 20:12:37 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/nixos/restic.nix
+++ nixos/modules/nixos/restic.nix
@@ -1,7 +1,16 @@
 { pkgs, ... }: {
+  # https://github.com/creativeprojects/resticprofile/issues/645
+  nixpkgs.overlays = [
+    (final: prev: {
+      resticprofile = prev.resticprofile.overrideAttrs (old: {
+        doCheck = false;
+      });
+    })
+  ];
+
   users.users."filo".packages = with pkgs; [
     restic
-    # resticprofile
+    resticprofile
     rclone
   ];
 }
