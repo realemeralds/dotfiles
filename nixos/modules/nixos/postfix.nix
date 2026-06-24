5f8400cbee218e89df8136eb1d325f84d9034a1f
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-24 18:40:08 +0800

    "294 2026-06-24 18:40:06 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/nixos/postfix.nix
+++ nixos/modules/nixos/postfix.nix
@@ -8,7 +8,8 @@
     enable = true;
     settings.main = {
       relayhost = [ "[smtp.gmail.com]:465" ];
-      smtp_use_tls = "yes";
+      myhostname = "test";
+      # smtp_use_tls = "yes";
       smtp_sasl_auth_enable = "yes";
       smtp_tls_wrappermode = "yes";
       smtp_tls_security_level = "encrypt";
