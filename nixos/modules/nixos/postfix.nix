dac3ce8aff090e4ab7dd6f61b6465a48938bae3c
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-24 18:23:23 +0800

    "285 2026-06-24 18:23:21 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/nixos/postfix.nix
+++ nixos/modules/nixos/postfix.nix
@@ -11,6 +11,7 @@
       relayhost = [ "[smtp.gmail.com]:587" ];
       smtp_tls_wrappermode = "yes";
       smtp_tls_security_level = "encrypt";
+      smtp_tls_security_options = "noanonymous";
       smtp_sasl_auth_enable = "yes";
       smtp_sasl_password_maps = "texthash:${config.sops.secrets."postfix/sasl_passwd".path}";
       # optional: Forward mails to root (e.g. from cron jobs, smartd)
