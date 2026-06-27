6dbc451a191d835c7b80c1b2ece119574ea6dffa
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-27 09:59:43 +0800

    "22 2026-06-27 09:59:40 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/nixos/greeter.nix
+++ nixos/modules/nixos/greeter.nix
@@ -7,7 +7,7 @@
       package = (pkgs.papirus-nord.override { accent = "frostblue4"; });
     };
     font = {
-      name = "Iosevka Nerd Font Mono 12";
+      name = "Iosevka Nerd Font Mono 11";
       package = pkgs.nerd-fonts.iosevka;
     };
     extraConfig = ''
