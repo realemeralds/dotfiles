a4d207b041cfca1f909d5b71c07e20217942eacf
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-27 09:45:01 +0800

    "17 2026-06-27 09:44:57 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/nixos/greeter.nix
+++ nixos/modules/nixos/greeter.nix
@@ -1,9 +1,10 @@
-{
+{ pkgs, ... }: {
   services.xserver.displayManager.lightdm.greeters.slick = {
     enable = true;
     draw-user-backgrounds = true;
     font = {
-      name = "Google Sans 11";
+      name = "Iosevka Nerd Font Mono 11";
+      package = pkgs.nerd-fonts.iosevka;
     };
   };
 }
