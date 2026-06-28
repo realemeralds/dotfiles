19ed047cf4ea86928adf01fa84a080fbe1f74395
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-06-28 21:41:54 +0800

    "27 2026-06-28 21:41:51 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/nixos/moonlight.nix
+++ nixos/modules/nixos/moonlight.nix
@@ -2,4 +2,15 @@
   users.users."filo".packages = with pkgs; [
     moonlight-qt
   ];
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-vaapi-driver
+      intel-media-driver
+      vpl-gpu-rt # for newer GPUs on NixOS >24.05 or unstable
+      # onevpl-intel-gpu  # for newer GPUs on NixOS <= 24.05
+      # intel-media-sdk   # for older GPUs
+    ];
+  };
 }
