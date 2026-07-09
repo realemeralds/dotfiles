c61985fd6a0d0e08ee2b17f4e96ee300e4c7cfa5
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-07-09 20:59:54 +0800

    filo@nixos "361 2026-07-09 20:59:51 26.11.20260610.9ae611a 6.18.35"

--- nixos/modules/home_manager/zen.nix
+++ nixos/modules/home_manager/zen.nix
@@ -72,13 +72,18 @@ in
         "zen.view.use-single-toolbar" = false;
         "zen.welcome-screen.seen" = true;
         "zen.workspaces.continue-where-left-off" = true;
+
+        # Zen Mod Settings
+        "theme.custom_uifont.custom" = "sans-serif";
+        "theme.custom_uifont.default" = "Serif";
+        "theme.custom_uifont.shadow" = "none";
+        "psu.tab_title_fixes.font_size" = "13px";
+        "psu.tab_title_fixes.pending_opacity" = "0.55";
       };
 
-      # Mod settings are NOT declarative.
       mods = [
         "e74cb40a-f3b8-445a-9826-1b1b6e41b846" # Custom uiFont
         "7190e4e9-bead-4b40-8f57-95d852ddc941" # Tab Title Fixes
-        "642854b5-88b4-4c40-b256-e035532109df" # Transparent Zen
       ];
 
       keyboardShortcutsVersion = 19;
