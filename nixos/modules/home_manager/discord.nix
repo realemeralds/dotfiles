38c27e6694b8b4e7bd39b161276a5caf24ca898a
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-07-10 13:14:52 +0800

    filo@nixos "368 2026-07-10 13:14:49 26.11.20260705.d407951 6.18.38"

--- nixos/modules/home_manager/discord.nix
+++ nixos/modules/home_manager/discord.nix
@@ -50,7 +50,11 @@
         clearUrls.enable = true;
         noBlockedMessages.enable = true;
         youtubeAdblock.enable = true;
-        customSounds.enable = true; # not declarative!
+        customSounds = {
+          enable = true;
+          callCalling = "{\"enabled\":true,\"selectedSound\":\"custom\",\"volume\":100,\"useFile\":false,\"selectedFileId\":\"75f26f84-b836-423b-ae34-32ada2dbe669\"}";
+          callRinging = "{\"enabled\":true,\"selectedSound\":\"custom\",\"volume\":100,\"useFile\":false,\"selectedFileId\":\"87fd4a3e-b5b8-4a01-9b27-ed837dcf1db8\"}";
+        }; # not declarative!
       };
     };
   };
