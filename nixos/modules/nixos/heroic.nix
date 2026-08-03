7a6ae756a2cb52390d759f78fbb2cf87140b311a
Matt C. <102229990+realemeralds@users.noreply.github.com>
2026-08-03 21:48:28 +0800

    filo@nixos "385 2026-08-03 21:48:26 26.11.20260705.d407951 6.18.38"

--- nixos/modules/nixos/heroic.nix
+++ nixos/modules/nixos/heroic.nix
@@ -1,14 +1,5 @@
 { pkgs, ... }: {
-  programs.gamescope.enable = true;
-  programs.gamemode.enable = true;
-
   users.users."filo".packages = with pkgs; [
-    (heroic.override {
-      extraPkgs =
-        pkgs': with pkgs'; [
-          gamescope
-          gamemode
-        ];
-    })
+    heroic
   ];
 }
